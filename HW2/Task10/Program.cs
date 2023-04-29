70e524c3714884db4942b5b28cf204193e4a7c48
Stanislav Brigadin <brigadins@gmail.com>
2023-04-29 22:22:09 +0500

    Add Homework_2 (29.04.23)

---
+++ HW2/Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трёхзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 1000 & a > 99)
+{
+    int result = a % 100 / 10;
+    Console.WriteLine("Вторая цифра " + result);
+}
+else if (a > -1000 & a < -99)
+{
+    int result = a % 100 / -10;
+    Console.WriteLine("Вторая цифра " + result);
+}
+else Console.WriteLine("Некорректное число");
+
---
+++ HW2/Task13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//  645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a > 999 || a < -999)
+{   
+   a = a / 10;
+}
+if (a > 99)
+{
+    int result = a % 10;
+    Console.WriteLine(result);
+}
+else if (a < -99)
+{
+    int result = a % 10 * -1;
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Третьей цифры нет");
+
+// var number = 1234567;
+// var digitArray = number.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+
+
+// Console.WriteLine(digitArray[2]);
