679ee430b85c2877f234b4cc214258f4b45e4611
Stanislav Brigadin <brigadins@gmail.com>
2023-06-06 22:35:27 +0900

    Add Homework_6 (06.06.23)

---
+++ Homeworks/HW6_20230604/Task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел хотите ввести?");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int [] inputArray(int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"Введите {i+1}-е число ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int [] array = inputArray(count);
+Console.WriteLine($"Ваш массив ");
+Console.Write(String.Join(",", array));
+Console.WriteLine($" ");
+
+int schet = 0;
+for (int i = 0; i < count; i++)
+{
+    if (array[i] > 0)
+    schet++;
+}
+Console.WriteLine($"Чисел больше 0 = {schet} ");
---
+++ Homeworks/HW6_20230604/Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// 1) Ввести коэффициенты для 2х прямых
+// 2) написать функцию, вычисляющую общую точку
+// 3) вывод
+
+Console.WriteLine("Введите коэффициент k1 для первой прямой: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b1 для первой прямой: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k2 для второй прямой: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2 для второй прямой: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Уравнение первой прямой y = {k1}*x + {b1} ");
+Console.WriteLine($"Уравнение второй прямой y = {k2}*x + {b2} ");
+void ComPoint(double k1, double b1, double k2, double b2)
+{
+    double x = 0; // координата X общей точки
+    double y = 0; // координата Y общей точки
+    x = (b2-b1)/(k1-k2);
+    y = k1 * x + b1; 
+    if (k1 * x + b1 == k2 * x + b2 && k1 != k2)
+    {
+        Console.WriteLine($"Прямые пересекаются в точке ({x};{y})");
+    }
+    else  Console.WriteLine("Прямые не пересекаются");
+}
+ComPoint(k1, b1, k2, b2);
