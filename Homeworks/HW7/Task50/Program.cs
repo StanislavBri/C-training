f208aec334f33f24c8a489ffaea210f18f9f4657
Stanislav Brigadin <brigadins@gmail.com>
2023-06-09 20:43:20 +0900

    Add Homework_7 (09.06.23)

---
+++ Homeworks/HW7/Task50/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, 
+// и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите количество строк двумерного массива m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк двумерного массива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] Getarray2d(int m, int n)
+{
+    int [,] array2d = new int[m,n];
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            array2d[i, j] = new Random().Next(-10, 11);
+            Console.Write(array2d[i,j] + "  ");
+        }
+        Console.WriteLine();
+    }
+    return array2d;
+}
+int [,] array2d = Getarray2d(m,n);
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void FindNumber(int [,] array2d, int N)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            if (N == array2d[i,j])
+            Console.WriteLine($"Число находится на позиции {i},{j}");
+        }
+    }
+}
+FindNumber(array2d, N);
---
+++ Homeworks/HW7/Task52/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите количество строк двумерного массива m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] Getarray2d(int m, int n)
+{
+    int [,] array2d = new int[m,n];
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            array2d[i, j] = new Random().Next(-10, 11);
+            Console.Write(array2d[i,j] + "  ");
+        }
+        Console.WriteLine();
+    }
+    return array2d;
+}
+int [,] array2d = Getarray2d(m,n);
+
+
+void SrArif(int [,] array2d)
+{
+    //double SrArif = 0;
+    double [] sum = new double[n];
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            sum[j] = 0;
+            for (int i = 0; i < array2d.GetLength(0); i++)
+            {
+                // Console.WriteLine($" sum[k] = array2d[i,j] {sum[k]}");
+                sum[j] = sum[j] + array2d[i,j];
+            }
+            sum[j] = sum[j] / 2;
+        }    
+    Console.Write("Среднее арифметические столбцов = ");
+    Console.WriteLine(String.Join("; ", sum));
+}
+SrArif(array2d);
